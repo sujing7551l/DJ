c65df0473cbad220a28e30c715c02d080c55bbcf
sjj <15472747@qq.com>
2019-11-18 10:46:23 +0800

    two

---
+++ wyy/xz.sql
@@ -0,0 +1,26 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS xz;
+CREATE DATABASE xz CHARSET=UTF8;
+USE xz;
+
+
+/**用户信息**/
+CREATE TABLE xz_user(
+  uid INT PRIMARY KEY AUTO_INCREMENT,
+  uname VARCHAR(32),
+  upwd VARCHAR(32),
+  email VARCHAR(64),
+  phone VARCHAR(16),
+
+  avatar VARCHAR(128),        #头像图片路径
+  user_name VARCHAR(32),      #用户名
+  gender INT                  #性别  0-女  1-男
+);
+
+
+/**用户信息**/
+INSERT INTO xz_user VALUES
+(NULL, 'dingding', '123456', 'ding@qq.com', '13501234567', 'img/avatar/default.png', '丁伟', '1'),
+(NULL, 'dangdang', '123456', 'dang@qq.com', '13501234568', 'img/avatar/default.png', '林当', '1'),
+(NULL, 'doudou', '123456', 'dou@qq.com', '13501234569', 'img/avatar/default.png', '窦志强', '1'),
+(NULL, 'yaya', '123456', 'ya@qq.com', '13501234560', 'img/avatar/default.png', '秦小雅', '0');
